5cf2072418e5ec4947fc2d07368fddebda19a7a0
gicu204 <84690846+gicu204@users.noreply.github.com>
2023-09-25 11:15:21 +0300

    Update tema2.c

--- tema2.c
+++ tema2.c
@@ -3,6 +3,30 @@
 #include<conio.h>
 #include<math.h>
 #include<time.h>
+void quicksort(int number[], int first, int last){
+int pivot, i, j, temp;
+if(first < last){
+    pivot = first;
+    i=first;
+    j=last;
+    while(i < j){
+        while(number[i] <= number[pivot] && i < last)
+            i++;
+            while(number[j] > number[pivot])
+            j--;
+            if(i < j){
+                temp = number[i];
+                number[i] = number[j];
+                number[j] = temp;
+            }
+    }
+    temp = number[pivot];
+    number[pivot] = number[j];
+    number[j] = temp;
+    quicksort(number, first, j-1);
+    quicksort(number, j+1, last);
+}
+}
 int main()
 {
     int n, i;
@@ -136,6 +160,32 @@ time_taken = ((double)m)/CLOCKS_PER_SEC;
     for(i = 0; i < n; i++){
         printf(" %d", b[i]);
     }
+//
+
+for (i = 0; i < n; i++){
+        b[i]=a[i];
+    }
+    printf(" \n \n");
+
+
+//Quick-sort
+m = clock();
+
+quicksort(b, 0, n-1);
+
+m = clock() - m;
+time_taken = ((double)m)/CLOCKS_PER_SEC;
+
+    printf("the array sorted by Quick-sort in %f seconds: ", time_taken);
+    for(i = 0; i<n; i++){
+        printf(" %d", b[i]);
+    }
+
+//
+    for (i = 0; i < n; i++){
+        b[i]=a[i];
+    }
+    printf(" \n \n");
 
 
 
